f6add4d75b9804e43f28e883fe02a87075241dec
theCasey <141908965+theCasey-k@users.noreply.github.com>
2026-02-06 14:55:20 +0530

    Initial commit: Flask CRUD application

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask,render_template,request,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+
+
+#Creating object
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///employee.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATION"]=False
+
+db=SQLAlchemy(app)
+app.app_context().push()
+
+class Employee(db.Model):
+    sno=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(200),nullable=False)
+    email=db.Column(db.String(500),nullable=False)
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        name=request.form['name']
+        email=request.form['email']
+        employee=Employee(name=name,email=email)
+        db.session.add(employee)
+        db.session.commit()
+    allemployee=Employee.query.all()
+    return render_template("index.html",allemployee=allemployee)
+
+@app.route("/about")
+def about(): 
+    return render_template("about.html")
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    employee=Employee.query.filter_by(sno=sno).first()
+    db.session.delete(employee)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route("/update/<int:sno>", methods=['GET', 'POST'])
+def update(sno):
+    employee=Employee.query.filter_by(sno=sno).first()
+    if request.method == 'POST':
+        employee.name=request.form['name']
+        employee.email=request.form['email']
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("update.html",employee=employee)
+
+@app.route("/delete_multiple", methods=['POST'])
+def delete_multiple():
+    employee_ids = request.form.getlist('employee_ids')
+    if employee_ids:
+        for emp_id in employee_ids:
+            employee = Employee.query.filter_by(sno=int(emp_id)).first()
+            if employee:
+                db.session.delete(employee)
+        db.session.commit()
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
